88dff9626d780bc08b329b145eb9b910dd3c8f58
Vincent Lemstra <vincentlemstra@gmail.com>
2023-02-08 09:47:51 +0100

    add static data: UserRoles

---
+++ VacIT/Data/Static/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace VacIT.Data.Static
+{
+    public static class UserRoles
+    {
+        public const string Admin = "admin";
+        public const string User = "user";
+        public const string Employer = "employer";
+    }
+}
