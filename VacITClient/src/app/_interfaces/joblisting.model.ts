6d35e5c5dc68d7e990e93080c0828db03a95aba7
Vincent Lemstra <vincentlemstra@gmail.com>
2023-02-14 16:12:43 +0100

    update models

--- VacITClient/src/app/_interfaces/joblisting.model.ts
+++ VacITClient/src/app/_interfaces/joblisting.model.ts
@@ -1,6 +1,9 @@
+import { Employer } from "./employer.model";
+
 export interface JobListing {
     id: string;
     employerId: string;
+    employer: Employer;
     logoURL: string;
     name: string;
     level: string;
